883da069783fda3775ed34af5faf60677d7e669b
Ashish Tummuri <68654310+ashishtummuri@users.noreply.github.com>
2025-08-14 12:26:15 -0400

    Delete lab11a_rm_gen directory

--- lab11a_rm_gen/reg_verifier_dir/uvmreg/xcelium.d/run.lnx8664.25.03.d/.timestamp.ts
+++
@@ -1,3 +0,0 @@
-1754942741 /mnt/ncsudrive/a/atummur/PROJECTS/uvma_training_1.2.6rev1/uvm/lab11a_rm_gen/reg_verifier_dir/uvmreg/cdns_uvmreg_utils_pkg.sv
-1754943153 /mnt/ncsudrive/a/atummur/PROJECTS/uvma_training_1.2.6rev1/uvm/lab11a_rm_gen/reg_verifier_dir/uvmreg/quicktest.sv
-1754942740 /mnt/ncsudrive/a/atummur/PROJECTS/uvma_training_1.2.6rev1/uvm/lab11a_rm_gen/reg_verifier_dir/uvmreg/yapp_router_regs_rdb.sv
